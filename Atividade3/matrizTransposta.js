5a20ce61f441ed153ae81978e38b66ceb39bfd62
pedroserviio <pedroservio2010@gmail.com>
2024-03-21 19:34:30 -0300

    Atividade 3 Programação para Web

---
+++ Atividade3/matrizTransposta.js
@@ -0,0 +1,36 @@
+const matriz = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+
+function transporMatriz(A) {
+    function imprimirMatriz(matriz) {
+        for (let i = 0; i < matriz.length; i++) {
+            console.log(matriz[i].join('\t'));
+        }
+    }
+
+    function transpor(A) {
+        const transposta = [];
+        const linhas = A.length;
+        const colunas = A[0].length;
+
+        for (let j = 0; j < colunas; j++) {
+            transposta[j] = [];
+            for (let i = 0; i < linhas; i++) {
+                transposta[j][i] = A[i][j];
+                //console.log('Transposta[j][i]' + transposta[j][i]);
+            }
+        }
+
+        return transposta;
+    }
+
+    console.log("Matriz original:");
+    imprimirMatriz(A);
+    console.log("\nMatriz transposta:");
+    imprimirMatriz(transpor(A));
+}
+
+transporMatriz(matriz);
